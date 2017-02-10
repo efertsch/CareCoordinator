ae391cd18ee179e4eb1107f57a17c6f570b16fa4
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-02-09 19:18:34 -0800

    refactors bcrypt password getter and setter methods and authentication for doctors and patients

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,14 +4,20 @@ class Doctor < ActiveRecord::Base
   has_many :patients, through: :doctor_reviews
   has_and_belongs_to_many :specialties
 
-  include BCrypt
+  validates :email, uniqueness: true 
 
-  def password
-    @password ||= Password.new(password_hash)
-  end
+  def password 
+    @password ||= BCrypt::Password.new(password_hash)
+  end 
 
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
+  def password=(plain_text_password)
+    @password = BCrypt::Password.create(plain_text_password)
+    self.password_hash = @password 
+  end 
+
+  def self.authenticate(email, password)
+    valid_user = User.find_by(email: email)
+    valid_user.password == password ? valid_user : nil 
+  end 
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,14 +1,21 @@
 class Patient < ActiveRecord::Base
   has_many :doctor_reviews
   has_many :doctors, through: :doctor_reviews
-  include BCrypt
+  
+  validates :email, uniqueness: true 
 
-  def password
-    @password ||= Password.new(password_hash)
-  end
+  def password 
+    @password ||= BCrypt::Password.new(password_hash)
+  end 
 
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
+  def password=(plain_text_password)
+    @password = BCrypt::Password.create(plain_text_password)
+    self.password_hash = @password 
+  end 
+
+  def self.authenticate(email, password)
+    valid_user = User.find_by(email: email)
+    valid_user.password == password ? valid_user : nil 
+  end 
+
 end
